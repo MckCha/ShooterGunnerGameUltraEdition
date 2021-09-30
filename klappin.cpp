44ea783c4d2bdc486824d4b7b0bed1bf3075dfb6
Nofunzoner <nofunzone2@gmail.com>
2021-09-30 11:12:49 -0700

    Create klappin.cpp

---
+++ klappin.cpp
@@ -0,0 +1,13 @@
+#include "fonts.h"
+
+void display_credits(int x, int y)
+{
+	Rect r;
+	r.bot = y;
+	r.left = x;
+	r.center = 1;
+	ggprint8b(&r, 16, 0x00000000, "Connor Tennison");
+	ggprint8b(&r, 20, 0x00ffff00, "Eduardo Ramos Soriano");
+	ggprint8b(&r, 24, 0x00ff00ff, "Keith Lappin");
+	ggprint8b(&r, 28, 0xffff0000, "Mike Cha");
+}
