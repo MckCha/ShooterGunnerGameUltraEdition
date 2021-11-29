ca08a89ab2aff1cf0b84e67261ab3fcb5b18881f
Nofunzoner <nofunzone2@gmail.com>
2021-11-28 18:17:39 -0800

    Update myglobal.h

--- myglobal.h
+++ myglobal.h
@@ -6,6 +6,36 @@
 
 #ifndef _x_MYGLOBAL
 #define _x_MYGLOBAL
+
+
+//defined types
+typedef double Flt;
+typedef double Vec[3];
+typedef Flt	Matrix[4][4];
+
+
+
+//macros
+#define rnd() (((double)rand())/(double)RAND_MAX)
+#define random(a) (rand()%a)
+#define MakeVector(x, y, z, v) (v)[0]=(x),(v)[1]=(y),(v)[2]=(z)
+#define VecZero(v) (v)[0]=0.0,(v)[1]=0.0,(v)[2]=0.0
+#define VecCopy(a,b) (b)[0]=(a)[0];(b)[1]=(a)[1];(b)[2]=(a)[2]
+#define VecDot(a,b)	((a)[0]*(b)[0]+(a)[1]*(b)[1]+(a)[2]*(b)[2])
+#define VecSub(a,b,c) (c)[0]=(a)[0]-(b)[0]; \
+                      (c)[1]=(a)[1]-(b)[1]; \
+                      (c)[2]=(a)[2]-(b)[2]
+//constants
+const float timeslice = 1.0f;
+const float gravity = -0.4f;
+const int MAX_ENEMIES = 5;
+const int MAX_ENEMYLAS = 10;
+const int MAX_PLAYERLAS = 5;
+#define ALPHA 1
+
+int SCORE;
+
+
 class Global {
 public:
 	int done;
