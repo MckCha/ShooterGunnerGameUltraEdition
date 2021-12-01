a67b1e108f668366a9182f46a56bb541f9189e6a
Mike Cha <80453440+MckCha@users.noreply.github.com>
2021-11-30 18:18:12 -0800

    Add files via upload

--- mcha.cpp
+++ mcha.cpp
@@ -29,10 +29,31 @@ void showCredits(int x,int y)
 Bigfoot::Bigfoot() {};
 Bigfoot bigfoot;
 
-Image img[4] = {
+Image img[6] = {
 //"./images/bigfoot.png",
 "./images/yep.png",
 "./images/space.png",
 "./images/forestTrans.png",
-"./images/umbrella.png"};
+"./images/umbrella.png",
+"./images/Startmenu.png",
+"./images/resolution.png"};
 //"./images/yep.png" };
+
+extern Global g;
+
+void createMenu() {
+    glBindTexture(GL_TEXTURE_2D, g.startMenu);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, img[4].width, img[4].height,
+									0, GL_RGB, GL_UNSIGNED_BYTE, img[4].data);
+}
+
+void createSettings() {
+    glBindTexture(GL_TEXTURE_2D, g.settingPage);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, img[5].width, img[5].height,
+									0, GL_RGB, GL_UNSIGNED_BYTE, img[5].data);
+}
+
