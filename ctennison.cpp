e560d4cf8eadc23c93fa8ee406c14f8c948a7977
ctennison <89542033+ctennison@users.noreply.github.com>
2021-12-02 14:59:23 -0800

    Update ctennison.cpp

--- ctennison.cpp
+++ ctennison.cpp
@@ -22,4 +22,37 @@ Global::Global() {
 //Global::~Global() {
 //		logClose();
 //	}
-Global g;
+//Global g;
+
+void showCredits(int x,int y)
+{
+    Rect r;
+    r.bot = y;
+    r.left = x;
+    r.center = 1;
+    ggprint16(&r, 16, 0x0000ff00, "*CREDITS*");
+    ggprint16(&r, 16, 0x0000ff00, "-------------------------");
+    ggprint16(&r, 16, 0x0000ff00, "    ");
+    ggprint16(&r, 16, 0x0000ff00, "Mike Cha");
+    ggprint16(&r, 16, 0x0000ff00, "    ");
+    ggprint16(&r, 16, 0x0000ff00, "Eduardo Ramos Soriano");
+    ggprint16(&r, 16, 0x0000ff00, "    ");
+    ggprint16(&r, 16, 0x0000ff00, "Keith Lappin");
+    ggprint16(&r, 16, 0x0000ff00, "    ");
+    ggprint16(&r, 16, 0x0000ff00, "Connor Tennison");
+    ggprint16(&r, 16, 0x0000ff00, "    ");
+    ggprint16(&r, 16, 0x0000ff00, "-------------------------");
+	
+	glBindTexture(GL_TEXTURE_2D, g.credits);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, img[1].width, img[1].height,
+									0, GL_RGB, GL_UNSIGNED_BYTE, img[1].data);
+}
+void genPlay() {
+		glBindTexture(GL_TEXTURE_2D, g.play);
+		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+		glTexImage2D(GL_TEXTURE_2D, 0, 3, img[1].width, img[1].height,
+									0, GL_RGB, GL_UNSIGNED_BYTE, img[1].data);
+}
