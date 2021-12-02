f70f3b4a9feee1702d9f95ae5a67195f1859477e
ctennison <89542033+ctennison@users.noreply.github.com>
2021-12-02 15:12:36 -0800

    Update mcha.cpp

--- mcha.cpp
+++ mcha.cpp
@@ -5,25 +5,7 @@
 #include <string.h>
 #include "log.h"
 #include <vector>
-void showCredits(int x,int y)
-{
-    Rect r;
-    r.bot = y;
-    r.left = x;
-    r.center = 1;
-    ggprint16(&r, 16, 0x00ff0000, "*CREDITS*");
-    ggprint16(&r, 16, 0x00ff0000, "-------------------------");
-    ggprint16(&r, 16, 0x00ff0000, "    ");
-    ggprint16(&r, 16, 0x00ff0000, "Mike Cha");
-    ggprint16(&r, 16, 0x00ff0000, "    ");
-    ggprint16(&r, 16, 0x00ff0000, "Eduardo Ramos Soriano");
-    ggprint16(&r, 16, 0x00ff0000, "    ");
-    ggprint16(&r, 16, 0x00ff0000, "Keith Lappin");
-    ggprint16(&r, 16, 0x00ff0000, "    ");
-    ggprint16(&r, 16, 0x00ff0000, "Connor Tennison");
-    ggprint16(&r, 16, 0x00ff0000, "    ");
-    ggprint16(&r, 16, 0x00ff0000, "-------------------------");
-}
+
 
 //Declares bigfoot Object.
 Bigfoot::Bigfoot() {};
