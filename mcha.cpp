aaa9beef536e37179749dfbe9247afe116d032d7
Mike Cha <80453440+MckCha@users.noreply.github.com>
2021-12-02 14:03:07 -0800

    Update mcha.cpp

--- mcha.cpp
+++ mcha.cpp
@@ -29,15 +29,16 @@ void showCredits(int x,int y)
 Bigfoot::Bigfoot() {};
 Bigfoot bigfoot;
 
-Image img[6] = {
-//"./images/bigfoot.png",
+Image img[9] = {
 "./images/yep.png",
 "./images/space.png",
 "./images/forestTrans.png",
 "./images/umbrella.png",
 "./images/Startmenu.png",
-"./images/resolution.png"};
-//"./images/yep.png" };
+"./images/resolution.png",
+"./images/resolution1.png",
+"./images/resolution2.png",
+"./images/resolution3.png"};
 
 extern Global g;
 
@@ -57,3 +58,26 @@ void createSettings() {
 									0, GL_RGB, GL_UNSIGNED_BYTE, img[5].data);
 }
 
+void createRes1() {
+    glBindTexture(GL_TEXTURE_2D, g.resolution1);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, img[6].width, img[6].height,
+									0, GL_RGB, GL_UNSIGNED_BYTE, img[6].data);
+}
+
+void createRes2() {
+    glBindTexture(GL_TEXTURE_2D, g.resolution1);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, img[7].width, img[7].height,
+									0, GL_RGB, GL_UNSIGNED_BYTE, img[7].data);
+}
+
+void createRes3() {
+    glBindTexture(GL_TEXTURE_2D, g.resolution1);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, img[8].width, img[8].height,
+									0, GL_RGB, GL_UNSIGNED_BYTE, img[8].data);
+}
