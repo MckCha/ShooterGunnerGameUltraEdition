44247edb69a77cb3724fdd079dc50c1ed0c0c90a
ctennison <89542033+ctennison@users.noreply.github.com>
2021-12-02 15:21:48 -0800

    Update eramossorian.cpp

--- eramossorian.cpp
+++ eramossorian.cpp
@@ -27,6 +27,7 @@ void displayHelp(int x,int y)
     ggprint16(&r, 16, 0x0000ff00, "(-> right key) - Move to the right");
     ggprint16(&r, 16, 0x0000ff00, "    ");
     ggprint16(&r, 16, 0x0000ff00, "(^ up key) - Move the rocket forward");
+    ggprint16(&r, 16, 0x0000ff00, "    ");
     ggprint16(&r, 16, 0x0000ff00, "* Hold down up key to accelerate *");
     ggprint16(&r, 16, 0x0000ff00, "    ");
     ggprint16(&r, 16, 0x0000ff00, "(v down key) - Move the rocket backward");
